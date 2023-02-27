9a3ef5daffcbfa7f51305e1b46a336fb9c304f72
Todd Morey <todd@toddmorey.com>
2023-02-26 20:49:04 -0600

    externalized near setup into a module

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-// Near wallet related imports
-import { setupWalletSelector } from '@near-wallet-selector/core';
-import { setupModal } from '@near-wallet-selector/modal-ui';
-import { setupNearWallet } from '@near-wallet-selector/near-wallet';
-import { Buffer } from 'buffer';
-import '@near-wallet-selector/modal-ui/styles.css';
-import * as nearAPI from 'near-api-js';
+import { selectorWallet, x_auth_token } from '../lib/nearsetup';
 
 // Imports for elements used for routes
 import Root from './routes/root';
@@ -25,65 +19,6 @@ import ContainerPage, { loader as containerLoader } from './routes/container';
 
 // Styles (index.css handles tailwindcss imports)
 import './index.css';
-
-// buffer pollyfill needed by Near Wallet Selector
-globalThis.Buffer = Buffer;
-
-const selector = await setupWalletSelector({
-  network: 'testnet',
-  modules: [setupNearWallet()],
-});
-
-const modal = setupModal(selector, {
-  contractId: 'auth.onmachina.testnet',
-});
-
-if (!selector.isSignedIn()) modal.show();
-
-const selectorWallet = await selector.wallet();
-const selectorAccount = (await selectorWallet.getAccounts()).shift(); // Get the 1st account
-
-const accountId = selectorAccount.accountId;
-const networkId = 'testnet';
-
-const config = {
-  networkId,
-  keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore(),
-  nodeUrl: 'https://rpc.testnet.near.org',
-};
-
-const near = await nearAPI.connect(config);
-const signer = near.connection.signer;
-const signerPublicKey = (await signer.getPublicKey(accountId, config.networkId)).toString();
-
-const account = new nearAPI.Account(near.connection, accountId);
-
-const nonce = (await account.getAccessKeys()).find((k) => k.public_key === signerPublicKey).access_key.nonce.toString();
-
-const accountData = Buffer.from(JSON.stringify({ id: accountId, nonce }));
-const signatureData = await signer.signMessage(accountData, accountId, networkId);
-
-const publicKey = Buffer.from(signatureData.publicKey.toString());
-const signature = Buffer.from(signatureData.signature);
-
-const credentials = Buffer.from(
-  JSON.stringify({
-    account: accountData.toString('base64'),
-    publicKey: publicKey.toString('base64'),
-    signature: signature.toString('base64'),
-  }),
-).toString('base64');
-
-const response = await fetch('https://auth.testnet.onmachina.io/auth/v1', {
-  method: 'GET',
-  headers: {
-    'x-auth-user': 'any',
-    'x-auth-key': credentials,
-  },
-});
-
-// Retrieve the x-auth-token
-const x_auth_token = response.headers.get('x-auth-token');
 
 const router = createBrowserRouter([
   {
