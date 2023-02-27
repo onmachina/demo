d6c320596054875ff7b7ec8343801c5df153e8f6
Todd Morey <todd@toddmorey.com>
2023-02-26 21:00:05 -0600

    removing top level awaits

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { selectorWallet, x_auth_token } from '../lib/nearsetup';
+import nearSetup from '../lib/nearsetup';
 
 // Imports for elements used for routes
 import Root from './routes/root';
@@ -20,71 +20,73 @@ import ContainerPage, { loader as containerLoader } from './routes/container';
 // Styles (index.css handles tailwindcss imports)
 import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: '/account/',
-    element: (
-      <Root>
-        <AccountPage wallet={selectorWallet} />
-      </Root>
-    ),
-  },
-  {
-    path: '/',
-    element: (
-      <Root>
-        <ShardPage />
-      </Root>
-    ),
-    loader: async ({ params }) => {
-      return shardLoader(params, x_auth_token);
+nearSetup().then(({ selectorWallet, x_auth_token }) => {
+  const router = createBrowserRouter([
+    {
+      path: '/account/',
+      element: (
+        <Root>
+          <AccountPage wallet={selectorWallet} />
+        </Root>
+      ),
     },
-    children: [
-      {
-        path: ':container',
-        element: <ContainerPage />,
-        loader: async ({ params }) => {
-          return containerLoader(params, x_auth_token);
-        },
-        children: [
-          {
-            path: 'upload',
-            action: uploadAction,
-            element: <Upload authKey={x_auth_token} />,
-          },
-          {
-            path: 'delete',
-            action: deleteContainerAction,
-            loader: async ({ params }) => {
-              return deleteContainerLoader(params, x_auth_token);
-            },
-            element: <DeleteContainer authKey={x_auth_token} />,
-          },
-          {
-            path: ':object',
-            element: <Details authKey={x_auth_token} />,
-            loader: async ({ params }) => {
-              return detailsLoader(params, x_auth_token);
-            },
-            children: [
-              {
-                path: 'delete',
-                loader: async ({ params }) => {
-                  return deleteObjectLoader(params, x_auth_token);
-                },
-                element: <DeleteObject authKey={x_auth_token} />,
-                action: deleteObjectAction,
-              },
-            ],
-          },
-        ],
+    {
+      path: '/',
+      element: (
+        <Root>
+          <ShardPage />
+        </Root>
+      ),
+      loader: async ({ params }) => {
+        return shardLoader(params, x_auth_token);
       },
-    ],
-  },
-]);
+      children: [
+        {
+          path: ':container',
+          element: <ContainerPage />,
+          loader: async ({ params }) => {
+            return containerLoader(params, x_auth_token);
+          },
+          children: [
+            {
+              path: 'upload',
+              action: uploadAction,
+              element: <Upload authKey={x_auth_token} />,
+            },
+            {
+              path: 'delete',
+              action: deleteContainerAction,
+              loader: async ({ params }) => {
+                return deleteContainerLoader(params, x_auth_token);
+              },
+              element: <DeleteContainer authKey={x_auth_token} />,
+            },
+            {
+              path: ':object',
+              element: <Details authKey={x_auth_token} />,
+              loader: async ({ params }) => {
+                return detailsLoader(params, x_auth_token);
+              },
+              children: [
+                {
+                  path: 'delete',
+                  loader: async ({ params }) => {
+                    return deleteObjectLoader(params, x_auth_token);
+                  },
+                  element: <DeleteObject authKey={x_auth_token} />,
+                  action: deleteObjectAction,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-);
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  );
+});
