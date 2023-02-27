60e19edaa5590bb17e1384c79ae1bab758473156
Todd Morey <todd@toddmorey.com>
2023-02-26 20:20:04 -0600

    trying a new vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://auth.testnet.onmachina.io/auth/v1',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-    cors:false,
+  define: {
+    global: 'globalThis',
   },
   build: {
-    target: 'esnext'
-  }
-})
+    commonjsOptions: { include: [] },
+  },
+  optimizeDeps: {
+    disabled: false,
+  },
+});
