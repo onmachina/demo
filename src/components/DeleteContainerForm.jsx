964a446bb780d61fa504d6e2cc5d58b17e137d7a
Todd Morey <todd@toddmorey.com>
2023-06-08 10:15:48 -0500

    container section migration complete

--- src/components/DeleteContainerForm.jsx
+++ src/components/DeleteContainerForm.jsx
@@ -1,15 +1,37 @@
+import { useEffect, useState } from 'react';
 import { Link, Form } from 'react-router-dom';
 import { formatFileSize } from '../../lib/utils';
+import { fetchContainerMetadata } from '../../lib/onmachina';
 
 export default function DeleteContainerForm({ containerName, authToken, accountID }) {
-  const objectCount = 20;
+  const [metadata, setMetadata] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const metadata = await fetchContainerMetadata(containerName, accountID, authToken);
+        setMetadata(metadata);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [containerName]);
+
+  if (loading) return;
+
   return (
     <div className="w-2/4">
       <div className="drop-shadow-xl border border-red-400 bg-red-100 p-5 mb-5">
         <h2 className="border-b pb-2 mb-2 border-red-400 text-red-700">Delete {containerName}</h2>
         <p>
-          Are you sure you want to delete the container <strong>{containerName}</strong>? It contains {objectCount}{' '}
-          objects totalling {formatFileSize(23000)} in data. This action cannot be undone.
+          Are you sure you want to delete the container <strong>{containerName}</strong>? It contains{' '}
+          {metadata['x-container-object-count']} objects totalling {formatFileSize(metadata['x-container-bytes-used'])}{' '}
+          in data. This action cannot be undone.
         </p>
         <Form method="POST" action={`/`}>
           <input name="action" type="hidden" value="Delete Container" />
--- src/components/NewContainerForm.jsx
+++ src/components/NewContainerForm.jsx
@@ -14,41 +14,45 @@ export default function NewContainerForm({ authToken, accountID }) {
   };
 
   return (
-    <Form className="pt-4" autoComplete="off" method="POST" action={`/`}>
-      <div className="mb-6">
-        <input name="action" type="hidden" defaultValue="Create Container" />
-        <input name="token" type="hidden" defaultValue={authToken} />
-        <input name="accountId" type="hidden" defaultValue={accountID} />
-        <label className="block mb-2 text-sm font-medium text-gray-900">Container name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          placeholder="my-new-container"
-          onChange={handleInputChange}
-          required
-          autoFocus
-        />
-      </div>
-      <div className="flex items-start mb-6">
-        <label className="relative inline-flex items-center mb-4 cursor-pointer">
-          <input type="checkbox" value="" className="sr-only peer" name="public" id="public" />
-          <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
-          <span className="ml-3 text-sm font-medium text-gray-900">Make this container public</span>
-        </label>
-      </div>
-      <div className="flex items-center">
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-        >
-          Submit
-        </button>
-        <Link to="/" className="ml-4 text-sm text-gray-500 hover:text-gray-700">
-          Cancel
-        </Link>
-      </div>
-    </Form>
+    <div className="drop-shadow-xl border border-cyan-400 bg-cyan-100 p-5 mb-5">
+      <h2 className="border-b pb-2 mb-2 border-cyan-400">Create a new container</h2>
+
+      <Form className="pt-4" autoComplete="off" method="POST" action={`/`}>
+        <div className="mb-6">
+          <input name="action" type="hidden" defaultValue="Create Container" />
+          <input name="token" type="hidden" defaultValue={authToken} />
+          <input name="accountId" type="hidden" defaultValue={accountID} />
+          <label className="block mb-2 text-sm font-medium text-gray-900">Container name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="my-new-container"
+            onChange={handleInputChange}
+            required
+            autoFocus
+          />
+        </div>
+        <div className="flex items-start mb-6">
+          <label className="relative inline-flex items-center mb-4 cursor-pointer">
+            <input type="checkbox" value="" className="sr-only peer" name="public" id="public" />
+            <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+            <span className="ml-3 text-sm font-medium text-gray-900">Make this container public</span>
+          </label>
+        </div>
+        <div className="flex items-center">
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            Submit
+          </button>
+          <Link to="/" className="ml-4 text-sm text-gray-500 hover:text-gray-700">
+            Cancel
+          </Link>
+        </div>
+      </Form>
+    </div>
   );
 }
