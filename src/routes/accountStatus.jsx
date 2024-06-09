fc977176b0bb49cab93507e0385364b6b5c21b18
Todd Morey <todd@toddmorey.com>
2024-06-09 10:44:52 -0500

    formatting cleanup

--- src/routes/accountStatus.jsx
+++ src/routes/accountStatus.jsx
@@ -70,7 +70,7 @@ const Incomplete = (props) => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           class="w-6 h-6"
         >
