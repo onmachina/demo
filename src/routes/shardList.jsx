9960bd94ee630baa1ba0aa7dd54bbba6986d8adf
Todd Morey <todd@toddmorey.com>
2024-06-09 10:43:41 -0500

    updates to lastest shards available

--- src/routes/shardList.jsx
+++ src/routes/shardList.jsx
@@ -3,18 +3,13 @@ import ShardCard from '../components/ShardCard';
 export default function ShardList() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-3xl font-bold">Available Shards</h1>
+      <h1 className="text-3xl font-bold text-white">Available Shards</h1>
       <div className="flex flex-row space-x-4 mt-4">
-        <ShardCard
-          name="testnet.onmachina"
-          decsription="The current test cluster for the OnMachina platform."
-          url="/"
-          isActive={true}
-        />
         <ShardCard
           name="global01.onmachina"
           decsription="Global production cluster for the OnMachina platform"
           url="/"
+          isActive={true}
         />
         <ShardCard
           name="eu01.onmachina"
