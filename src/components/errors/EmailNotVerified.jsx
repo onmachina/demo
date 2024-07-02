60b7a0a5c0ba7be851e6c85626fa523238483c10
Todd Morey <todd@toddmorey.com>
2024-07-01 22:50:26 -0500

    made button active

--- src/components/errors/EmailNotVerified.jsx
+++ src/components/errors/EmailNotVerified.jsx
@@ -29,8 +29,10 @@ export default function EmailNotVerified({ error }) {
         Look for an email sent with the subject "Verify your email" sent from decentralinfra.com. That email contains a
         link you can click to verify your account.
       </div>
-      <div className="p-4 w-full">
-        <ButtonLink to="/account-status">Check again</ButtonLink>
+      <div className="p-4 pb-0 w-full">
+        <ButtonLink bg="active" to="/account-status">
+          Check again
+        </ButtonLink>
       </div>
     </div>
   );
