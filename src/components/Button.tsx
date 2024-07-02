67841ff6c154bffaf6b619f54f7af780aeb6faa0
Todd Morey <todd@toddmorey.com>
2024-07-01 22:22:13 -0500

    cleaning up styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -26,13 +26,13 @@ export const Button = function (props: ButtonProps) {
       );
     case 'delete':
       return (
-        <button className={deleteStyles} {...props}>
+        <button className={deleteStyles} {...props} style={{ maxWidth: props.maxWidth }}>
           <HiXMark size={22} style={{ display: 'inline-block' }} /> {props.children}
         </button>
       );
     default:
       return (
-        <button className={normalStyles} {...props}>
+        <button className={normalStyles} {...props} style={{ maxWidth: props.maxWidth }}>
           {props.children}
         </button>
       );
@@ -41,13 +41,13 @@ export const Button = function (props: ButtonProps) {
 
 export const ButtonLink = function (props: ButtonProps) {
   const background = props.bg === 'active' ? 'bg-ui-active' : 'bg-ui-base';
-  const baseStyles = `mb-4 px-4 py-2 text-sm ${background} border rounded-sm shadow-md w-full hover:border-ui-active transition-all duration-300`;
+  const baseStyles = `inline-block mb-4 px-4 py-2 text-sm ${background} border rounded-sm shadow-md w-full hover:border-ui-active transition-all duration-300`;
   const normalStyles = `${baseStyles} border-ui-base text-ui-active`;
 
   const deleteStyles = `${baseStyles} border-red-500 text-red-500`;
 
   return (
-    <Link className={normalStyles} {...props}>
+    <Link className={normalStyles} {...props} style={{ maxWidth: props.maxWidth }}>
       {props.children}
     </Link>
   );
